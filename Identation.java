bc54d8a127650fd1339bac95026ce5035c93b93e
Nikitapatil2005 <nikitadhu999@gmail.com>
2025-10-12 00:18:57 +0530

    Basic hello world program

---
+++ Identation.java
@@ -0,0 +1,8 @@
+public class Identation {
+    public static void main(String[] args) {
+        {
+            System.out.println("The code block inside {} runs unconditionally and prints a message.");
+        }
+        System.out.println("Curly brackets {} group multiple statements into a single block, even without conditions.");
+    }
+}
